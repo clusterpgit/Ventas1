e1a4dbf2dae4f9d00dac12694f6bf3181fd661be
Randall Ramos <randall.saucedo@gmail.com>
2020-08-18 00:43:33 -0600

    Adding user register

---
+++ main.js
@@ -0,0 +1,20 @@
+const singupForm = document.querySelector('#singup-form');
+
+singupForm.addEventListener('submit', (e) => {
+    //alert("enviando");
+    e.preventDefault();
+
+    const email = document.querySelector('#singup-email').value;
+
+
+    const password = document.querySelector('#singup-password').value;
+
+    //metodo para crear el usuario
+
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log('registrado');
+        });
+
+});
