dca76b7c67a7c64ded0b92e4d5778d61ba01037a
Randall Ramos <randall.saucedo@gmail.com>
2020-08-22 01:13:27 -0600

    login desde index

--- mainPrincipal.js
+++ mainPrincipal.js
@@ -1,7 +1,32 @@
-const loginForm = document.querySelector('#login-form');
+/*const loginForm = document.querySelector('#login-form');
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     //alert('vamos a loguearnos');
     window.location.replace("login.html");
+});*/
+
+//constante para login
+const loginForm = document.querySelector('#login-form');
+//logout min35
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#login-email').value;
+    const password = document.querySelector('#login-password').value;
+    console.log(email, password);
+    //alert('asd');
+    //console.log(email, password);
+
+    auth2
+        .signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+
+            alert('Bienvenido Administrador');
+            if (email == "admin@gmail.com") {
+                window.location.replace("admin.html");
+            } else {
+                window.location.replace("vendedor.html");
+            }
+
+        });
 });
